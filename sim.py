91521f32a2c83a595eba772af9e7aeaeeaf7fb18
sl-matrix <sl-matrix@hotmail.com>
2019-04-18 14:47:31 -0500

    changes to control flow

---
+++ sim.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import os
+import sys
+import parser
+
+def main(argv):
+    print("start idiot!");
+    #seq = parser.get_data(argv[1]);
+    print("end idiot!");
+
+
+if __name__ == "__main__":
+    main(sys.argv);
