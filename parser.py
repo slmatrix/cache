83f63c5eaf4f1bbbf0d929345d795b746bdbd390
Zhongxiu Yang <zhongxiu.yang@utsa.edu>
2019-04-18 16:22:20 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -5,26 +5,22 @@
 
 import sys
 
-def get_data(infile):
-    pass
-
 
 def main():
     # fetch the path and name to a memory trace file
-    cache_file = open(sys.argv[1], "r")
-    # fetch the size and way of the cache
-    # cache_size = sys.argv[2]
-    # cache_way = sys.argv[3]
-    caches = []
-    for lines in cache_file:
-        line = lines.split()
-        line[0] = line[0][:-1]
-        t = tuple(line)
-        caches.append(t)
-        # set associative
-        # replacement: LRU policy (either use a queue or an extra field in the cache frame to record the access time stamp to each cache line)
-        # write: write-back policy
-    cache_file.close()
+    gen = get_data(sys.argv[1])
+    # print(gen.__next__())
+
+
+def get_data(filename):
+    with open(filename) as f:
+        for line in f:
+            try:
+                pc, md, va = line.split()
+                pc = pc[:-1]
+                yield pc, md, va
+            except ValueError:
+                print("Invalid value received")
 
 
 if __name__ == '__main__':
