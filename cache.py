82fa333b51623c8e63f125d1c6ffc92eecc8345a
sl-matrix <sl-matrix@hotmail.com>
2019-04-20 19:03:25 -0500

    fixed bugs with linked list initialization

--- cache.py
+++ cache.py
@@ -1,5 +1,5 @@
 class node:
-    def __init__(self, line, next):
+    def __init__(self, data, next):
         self.data = data;
         self.next = next;
 
@@ -9,23 +9,31 @@ class linked_list:
         self.head = head;
 
 
-class set:
+class line:
+    def __init__(self, valid, dirty, tag):
+        self.tag   = tag;
+        self.valid = valid;
+        self.dirty = dirty;
+        self.data  = [None] * 0x40
+
+
+class cache_set:
     def __init__(self, r_policy, n_way):
         self.n_way    = n_way;
         self.r_policy = r_policy;
         self.tags     = dict();
 
         #create and initialize the cache lines in the set
-        self.blocks = dummy = linked_list(None, None);
-        for line in range(0, n_way):
+        self.blocks = dummy = node(None, None);
+        for i in range(0, n_way):
             dummy.next = node(line(0, 0, 0), None);
             dummy = dummy.next;
 
     def search(self, tag):
         if tag in self.tags:                               #is line in the cache
             return True;
-        else:                                                 #need to make room 
-            replace(tag);
+        else:                                                 #need to make room
+            self.replace(tag);
             return False;
 
     def replace(self, tag):
@@ -35,11 +43,3 @@ class set:
             pass;
         elif self.r_policy == "FIFO":
             pass;
-
-
-class line:
-    def __init__(self, valid, dirty, tag):
-        self.tag   = tag;
-        self.valid = valid;
-        self.dirty = dirty;
-        self.data  = [None] * 0x40
