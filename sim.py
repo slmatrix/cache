4ddd3c65e0824b2aa20873f71aa6530eb1d004bd
sl-matrix <sl-matrix@hotmail.com>
2019-04-29 14:34:41 -0500

    changed cmd-line parameters to include cacheline after specifying data-file

--- sim.py
+++ sim.py
@@ -8,7 +8,7 @@ from cache  import cache_set
 from parser import get_data
 
 def main(argv):
-	#default cache configuration - in bits
+    #default cache configuration - in bits
     n_way     = int(math.log(0x10    , 0x2));                             #16-way
     line_sz   = int(math.log(0x40    , 0x2));                                #64B
     #cache_sz = int(math.log(0x400, 0x2));                                   #1KB
@@ -17,24 +17,25 @@ def main(argv):
     cache_sz  = int(math.log(0x100000, 0x2));                                #1MB
     #cache_sz = int(math.log(0x400000, 0x2));                                #4MB
     user_policy = "LRU";
-    if len(argv) > 2:                #change defaults to user supplied arguements
-    	n_way = int(math.log(int(argv[3]), 0x2));
-    	user_policy = argv[4];
-    	if argv[2] == "1KB":
-    		cache_sz = int(math.log(0x400, 0x2));
-    	elif argv[2] == "32KB":
-    		cache_sz = int(math.log(0x8000, 0x2));
-    	elif argv[2] == "256KB":
-    		cache_sz = int(math.log(0x40000, 0x2));
-    	elif argv[2] == "1MB":
-    		cache_sz = int(math.log(0x100000, 0x2));
-    	elif argv[2] == "4MB":
-    		cache_sz = int(math.log(0x400000, 0x2));
-    	else:
-	        pass
-    else:
-        pass
 
+    if len(argv) > 2:                #change defaults to user supplied arguements
+      line_sz = int(math.log(int(argv[2]), 2));
+
+      if argv[3] == "1KB":
+      	cache_sz = int(math.log(0x400, 0x2));
+      elif argv[3] == "32KB":
+      	cache_sz = int(math.log(0x8000, 0x2));
+      elif argv[3] == "256KB":
+      	cache_sz = int(math.log(0x40000, 0x2));
+      elif argv[3] == "1MB":
+      	cache_sz = int(math.log(0x100000, 0x2));
+      elif argv[3] == "4MB":
+      	cache_sz = int(math.log(0x400000, 0x2));
+
+      n_way = int(math.log(int(argv[4]), 0x2));
+      user_policy = argv[5];
+
+    print(line_sz, cache_sz, n_way, user_policy);
     miss   = 0
     access = 0
     cache  = []
